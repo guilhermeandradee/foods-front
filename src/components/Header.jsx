230e4c4c1e2f7944ef6c214fc58d9ccf03cf4689
Guilherme Andrade <96486731+guilhermeandradee@users.noreply.github.com>
2024-07-16 21:16:05 -0300

    last update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({ data }) => {
     return(
         <>
             <header className="container-fluid d-flex justify-content-center align-items-center" style={{backgroundColor: '#F6F7F8'}}>
-                <h1 className="fs-4 p-2" style={{color: "#715442"}}> Alimentos disponíveis ({ data.lenght == (0) ? 0 : data.length })</h1>
+                <h1 className="fs-4 p-2" style={{color: "#000"}}> Alimentos disponíveis ({ data.lenght == (0) ? 0 : data.length })</h1>
             </header>
         </>
     )
